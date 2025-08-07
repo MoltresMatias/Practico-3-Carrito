55560ffffb4932cfac33f03ce4d7915cd84f75c1
Matias <matiaspupirivarola@gmail.com>
2025-08-07 20:03:26 -0300

    elimine etiqueta seccion

--- src/Routing/Rutas.jsx
+++ src/Routing/Rutas.jsx
@@ -8,15 +8,13 @@ import Nosotros from "../components/Pages/Nosotros";
 const Rutas = () => {
   return (
     <BrowserRouter>
-      <section id="content" className="content">
-        <Routes>
-          <Route path="/" element={<Inicio />} />
-          <Route path="/inicio" element={<Inicio />} />
-          <Route path="/contacto" element={<Contacto />} />
-          <Route path="/carrito" element={<Carrito />} />
-          <Route path="/nosotros" element={<Nosotros />} />
-        </Routes>
-      </section>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/inicio" element={<Inicio />} />
+        <Route path="/contacto" element={<Contacto />} />
+        <Route path="/carrito" element={<Carrito />} />
+        <Route path="/nosotros" element={<Nosotros />} />
+      </Routes>
     </BrowserRouter>
   );
 };
