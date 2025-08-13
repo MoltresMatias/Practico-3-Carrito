530036dea3c0d00f911db74a21071bee7df26e4e
Matias <matiaspupirivarola@gmail.com>
2025-08-13 20:15:03 -0300

    modifique el tamaño del carrito

--- src/components/Pages/Carrito.jsx
+++ src/components/Pages/Carrito.jsx
@@ -1,17 +1,15 @@
 import { useCartStore } from "../stores/cartStore";
-import Navbar from "../Layout/Navbar";
 import CarritoProducto from "./CarritoProducto";
 import PrecioFinal from "./PrecioFinal";
 import "./Products.css";
-import Footer from "../Layout/Footer";
 
 const Carrito = () => {
   const carrito = useCartStore((state) => state.carrito);
   return (
     <>
-      <div className="d-flex justify-content-center container-fluid">
+      <div className="d-flex justify-content-center container">
         {carrito.length > 0 ? (
-          <div className="my-5 border rounded p-3 bg-white shadow">
+          <div className="my-5 border rounded p-3 bg-white shadow w-50">
             <h2 className="text-center fs-3 fw-bold text-danger border-bottom">
               Tu seleccion
             </h2>
@@ -20,7 +18,12 @@ const Carrito = () => {
             </div>
             <PrecioFinal />
             <div className="d-flex justify-content-center">
-              <button className="btn btn-danger mt-3 ">Comprar</button>
+              <button
+                onClick={() => window.location.reload()}
+                className="btn btn-danger mt-3 "
+              >
+                Comprar
+              </button>
             </div>
           </div>
         ) : (
